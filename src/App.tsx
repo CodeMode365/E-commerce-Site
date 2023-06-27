4194984d822c3f2ada7d51469d4b9ab2a8d14834
CodeMode365 <codemode365@gmail.com>
2023-06-27 18:12:39 +0545

    basic arrangements

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { MantineProvider } from '@mantine/core'
 import Navigation from './components/Navigation'
 import { Hero } from './components/Hero'
+import Markets from './components/Markets'
 
 const App = () => {
   return (
-    <MantineProvider withGlobalStyles theme={{ fontFamily: "Poppins, sans-serif", }}>
+    <MantineProvider withGlobalStyles theme={{ fontFamily: "Roboto, sans-serif", }}>
       <Navigation />
       <Hero />
+      <Markets />
     </MantineProvider>
   )
 }
