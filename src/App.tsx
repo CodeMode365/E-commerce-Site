df84ae9dab24a11887236e70e4de70fc01ba85b6
CodeMode365 <codemode365@gmail.com>
2023-06-28 20:50:38 +0545

    About basic

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Navigation from './components/Navigation'
 import { Hero } from './components/Hero'
 import Markets from './components/Markets'
 import ItemShowCase from './components/ItemShowCase'
+import About from './components/About'
 
 const App = () => {
   return (
     <MantineProvider withGlobalStyles theme={{ fontFamily: "Roboto, sans-serif", }}>
       <Navigation />
       <Hero />
+      <About />
       <Markets />
       <ItemShowCase />
     </MantineProvider>
