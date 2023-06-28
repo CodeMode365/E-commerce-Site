bf84ad0fdeedf3c391a3d627265d2d7e54d4b3fc
CodeMode365 <codemode365@gmail.com>
2023-06-28 21:03:53 +0545

    about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Container, Flex, Group, Text, Image, Title } from '@mantine/core'
+import { useTheme } from '@emotion/react'
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
+    const theme = useTheme()
+    return (
+        <Container my={30}>
+            <Flex>
+                <Image src={"./src/assets/clothes.jpg"} height={400} />
+                <Flex direction={"column"} ml={25}>
+                    <Title>About <Text display={"inline"} color={theme.colors.blue[7]}>Our Store</Text></Title>
+                    <Text align='justify'>
+
+                        &nbsp; &nbsp; Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus a ratione saepe aut harum tempora dolorum aperiam, voluptatem, nam molestiae eum possimus. Nobis, nam ad!
+                        <br />
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, eius. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, ea. Hic id error alias quisquam ducimus officiis tenetur unde tempore.
+                        <br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, doloribus.
+                    </Text>
+                </Flex >
+            </Flex>
+        </Container>
+    )
 }
 
 export default About
