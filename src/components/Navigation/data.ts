37106067ad39a41700d4dcfa3aaa3f77cc0a5c3e
CodeMode365 <codemode365@gmail.com>
2023-07-15 18:36:57 +0530

    data seperated

---
+++ src/components/Navigation/data.ts
@@ -0,0 +1,60 @@
+import { TbJacket } from "react-icons/tb"
+import { GiArmoredPants, GiMonclerJacket, GiBallerinaShoes } from "react-icons/gi"
+import { TfiShine } from "react-icons/tfi"
+import { AiTwotoneStar } from "react-icons/ai"
+import { RiShirtLine } from "react-icons/ri"
+import { BsGenderMale, BsGenderFemale } from "react-icons/bs"
+import { BiSolidTShirt } from "react-icons/bi"
+
+
+export const Categories = [{
+    icon: TfiShine,
+    title: 'New Arrivals',
+    description: 'This Pokémon’s cry is very loud and distracting',
+},
+{
+    icon: AiTwotoneStar,
+    title: 'Top Rated',
+    description: 'The fluid of Smeargle’s tail secretions changes',
+},
+{
+    icon: BsGenderMale,
+    title: 'Male',
+    description: 'The fluid of Smeargle’s tail secretions changes',
+},
+{
+    icon: BsGenderFemale,
+    title: 'Female',
+    description: 'The fluid of Smeargle’s tail secretions changes',
+},
+{
+    icon: BiSolidTShirt,
+    title: 'T-Shirts',
+    description: 'Yanma is capable of seeing 360 degrees without',
+},
+{
+    icon: GiArmoredPants,
+    title: 'Pants',
+    description: 'The shell’s rounded shape and the grooves on its.',
+},
+{
+    icon: RiShirtLine,
+    title: 'Shirts',
+    description: 'This Pokémon uses its flying ability to quickly chase',
+},
+{
+    icon: GiBallerinaShoes,
+    title: 'Shoes',
+    description: 'Combusken battles with the intensely hot flames it spews',
+},
+{
+    icon: TbJacket,
+    title: 'Jackets',
+    description: 'Combusken battles with the intensely hot flames it spews',
+},
+{
+    icon: GiMonclerJacket,
+    title: 'Hoody',
+    description: 'Combusken battles with the intensely hot flames it spews',
+},
+];
