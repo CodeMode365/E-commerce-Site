74893f5327374eed7c2d1cdc9963a59acb711f11
CodeMode365 <codemode365@gmail.com>
2023-07-15 18:37:06 +0530

    toggle navigation

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -3,75 +3,21 @@ import {
     Header, HoverCard, Button, Group, UnstyledButton, Text, SimpleGrid,
     ThemeIcon, Anchor, Divider, Center, Box, Burger, Drawer, Collapse, ScrollArea, rem
 } from "@mantine/core"
-import { BsArrowDownShort, BsGenderMale, BsGenderFemale } from "react-icons/bs"
 import { useDisclosure } from '@mantine/hooks'
 import { DiHeroku } from "react-icons/di"
-import { AiTwotoneStar } from "react-icons/ai"
-import { RiShirtLine } from "react-icons/ri"
-import { BiMoon, BiSolidTShirt, BiSun } from "react-icons/bi"
-import { TbJacket } from "react-icons/tb"
-import { GiArmoredPants, GiMonclerJacket, GiBallerinaShoes } from "react-icons/gi"
-import { TfiShine } from "react-icons/tfi"
+
+
 import { NavLink as NLink, NavLink } from 'react-router-dom'
 import { AppDispatch, RootState } from '../../Redux/store'
 import { useSelector, useDispatch } from 'react-redux'
 import { toggleMode } from '../../Redux/Slices/ColorMode'
 import { useLocation } from 'react-router-dom'
 import { useStyles } from './useStyles'
+import { Categories as mockdata } from './data'
+import { BsArrowDownShort } from 'react-icons/bs'
+import { BiMoon, BiSun } from 'react-icons/bi'
 
 
-
-const mockdata = [{
-    icon: TfiShine,
-    title: 'New Arrivals',
-    description: 'This Pokémon’s cry is very loud and distracting',
-},
-{
-    icon: AiTwotoneStar,
-    title: 'Top Rated',
-    description: 'The fluid of Smeargle’s tail secretions changes',
-},
-{
-    icon: BsGenderMale,
-    title: 'Male',
-    description: 'The fluid of Smeargle’s tail secretions changes',
-},
-{
-    icon: BsGenderFemale,
-    title: 'Female',
-    description: 'The fluid of Smeargle’s tail secretions changes',
-},
-{
-    icon: BiSolidTShirt,
-    title: 'T-Shirts',
-    description: 'Yanma is capable of seeing 360 degrees without',
-},
-{
-    icon: GiArmoredPants,
-    title: 'Pants',
-    description: 'The shell’s rounded shape and the grooves on its.',
-},
-{
-    icon: RiShirtLine,
-    title: 'Shirts',
-    description: 'This Pokémon uses its flying ability to quickly chase',
-},
-{
-    icon: GiBallerinaShoes,
-    title: 'Shoes',
-    description: 'Combusken battles with the intensely hot flames it spews',
-},
-{
-    icon: TbJacket,
-    title: 'Jackets',
-    description: 'Combusken battles with the intensely hot flames it spews',
-},
-{
-    icon: GiMonclerJacket,
-    title: 'Hoody',
-    description: 'Combusken battles with the intensely hot flames it spews',
-},
-];
 
 
 const Navigation: React.FC = () => {
@@ -216,7 +162,7 @@ const Navigation: React.FC = () => {
                 <ScrollArea h={`calc(100vh - ${rem(60)})`} mx="-md">
                     <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
 
-                    <NLink to="/" className={`${classes.link} ${isActiveLink('/') ? classes.activeLink : ''}`}>
+                    <NLink onClick={closeDrawer} to="/" className={`${classes.link} ${isActiveLink('/') ? classes.activeLink : ''}`}>
                         Home
                     </NLink>
                     <UnstyledButton className={`${classes.link} ${isActiveLink('/categories') ? classes.activeLink : ''}`} onClick={toggleLinks}>
@@ -228,17 +174,17 @@ const Navigation: React.FC = () => {
                         </Center>
                     </UnstyledButton>
                     <Collapse in={linksOpened}>{links}</Collapse>
-                    <NLink to="/products" className={`${classes.link} ${isActiveLink('/products') ? classes.activeLink : ''}`}>
+                    <NLink onClick={closeDrawer} to="/products" className={`${classes.link} ${isActiveLink('/products') ? classes.activeLink : ''}`}>
                         Products
                     </NLink>
-                    <NLink to="/cart" className={`${classes.link} ${isActiveLink('/cart') ? classes.activeLink : ''}`}>
+                    <NLink onClick={closeDrawer} to="/cart" className={`${classes.link} ${isActiveLink('/cart') ? classes.activeLink : ''}`}>
                         Cart <Center ml={5} bg={"red"} sx={{ borderRadius: 20, color: "white" }} w={25} h={25}>{cartItems.length}</Center>
                     </NLink>
 
                     <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
 
                     <Group position="center" grow pb="xl" px="md" fw={500}>
-                        <Button variant="default">
+                        <Button variant="default" onClick={closeDrawer}>
                             <NavLink to="/auth" className={classes.authBtn}>
                                 Log in
                             </NavLink>
