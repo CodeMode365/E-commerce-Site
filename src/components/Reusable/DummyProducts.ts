eef92f729b01f2bc323464d290dac1cea618cb0e
CodeMode365 <codemode365@gmail.com>
2023-07-15 20:46:25 +0530

    name shortended

--- src/components/Reusable/DummyProducts.ts
+++ src/components/Reusable/DummyProducts.ts
@@ -161,7 +161,7 @@ const Data: iCategory[] = [
             }
             , {
                 id: 400.8,
-                title: "Work Tshirt",
+                title: "Working Tshirt",
                 desc: "",
                 src: "./Items/Shirts/WorkingTshirt.jpg",
                 price: 30,
@@ -222,7 +222,7 @@ const Data: iCategory[] = [
             }
             , {
                 id: 500.6,
-                title: "Tigera Slipper",
+                title: "Slipper",
                 desc: "",
                 src: "./Items/Shoes/Slipper.jpg",
                 price: 30,
